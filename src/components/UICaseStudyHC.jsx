7e713df27d056c14551d4d4985fcf4ac873a0995
Christina dt <cnicodc@gmail.com>
2023-11-25 11:17:11 -0800

    intro headers

--- src/components/UICaseStudyHC.jsx
+++ src/components/UICaseStudyHC.jsx
@@ -17,15 +17,57 @@ const UICaseStudyHC = () => {
             </span>
           </button>
         </div>
+
         <div>
-          <img className='mx-auto p-4' src={hoodcats} alt='' />
+          <img className='mx-auto p-4 cursor-pointer' onClick={() => window.location.href = 'https://www.figma.com/proto/u3cuuuWqyHyOsbnLPuvQve/Pitch-Deck-Christina?type=design&node-id=0-1&t=g0tySnhpzsp8LF68-0&scaling=contain&starting-point-node-id=282%3A8487&show-proto-sidebar=1'} src={hoodcats} alt='' />
+        </div>
+      </div>
+
+      <div className='max-w-[1200px] w-full grid grid-cols-1 md:grid-cols-2 gap-8 py-8'>
+        <div>
+          <h1 className='text-2xl font-bold inline border-2 px-4 py-2 text-[#F5F5FA] border-pink-600'>Challenge</h1>
+          <p className='text-[#8892b0] py-4 max-w-[700px]'> Design a platform and User Interface for a non-profit that will allow users to pre-order food and beverages for pickup, schedule visits to the cat cafe, and load funds on to their account. </p>
+        </div>
+        <div>
+          <h1 className='text-2xl font-bold inline border-2 px-4 py-2 text-[#F5F5FA] border-pink-600'>Role</h1>
+          <p className='text-[#8892b0] py-4 max-w-[700px]'> User interface designer, solo project</p>
+        </div>
+      </div>
+
+      <div className='max-w-[1200px] w-full grid grid-cols-1 md:grid-cols-2 gap-24 py-8'>
+        <div>
+          <h1 className='text-2xl font-bold inline border-2 px-4 py-2 text-[#F5F5FA] border-pink-600'>Tools</h1>
+          <p className='text-[#8892b0] py-4 max-w-[700px]'>
+            <ul>
+              <li>Figma</li>
+              <li>Adobe Illustrator</li>
+              <li>Maze.co</li>
+              <li>Google Suite</li>
+              <li>Zoom</li>
+            </ul>
+          </p>
         </div>
 
-
-
+        <div>
+          <h1 className='text-2xl font-bold inline border-2 px-4 py-2 text-[#F5F5FA] border-pink-600'>Deliverables</h1>
+          <p className='text-[#8892b0] py-4 max-w-[700px]'>
+            <ul>
+              <li>UI prototype</li>
+              <li>User Research</li>
+              <li>Personas</li>
+              <li>Task Flows</li>
+            </ul>
+          </p>
+        </div>
 
       </div>
+      <div>
+        <h1 className='text-2xl font-bold inline border-2 px-4 py-2 text-[#F5F5FA] border-pink-600'>Timeline</h1>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>February 2023 - April 2023</p>
+      </div>
     </div>
+
+
   )
 }
 
