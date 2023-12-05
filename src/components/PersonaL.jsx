eadaf930870f8c57ef8deef10894924b8ef9f29d
Christina dt <cnicodc@gmail.com>
2023-12-04 16:17:29 -0800

    host persona from EE, missing img

--- src/components/PersonaL.jsx
+++ src/components/PersonaL.jsx
@@ -3,66 +3,67 @@ import laila from '../assets/images/Laila.png'
 
 const PersonaL = () => {
     return (
-        <div name="persona" className='w-full mx-auto px-8 sm:px-32 flex flex-col items-center justify-center bg-[#1F1F21]'>
-            <h1 className='sm:text-4xl font-bold inline border-4 px-4 py-2 mt-8 tracking-wider bg-[#1F1F21] border-pink-600 text-[#F5F5FA]'>Persona</h1>
-            <h1 className='sm:text-2xl font-bold inline border-2 px-4 py-2 mt-8 tracking-wider bg-[#1F1F21] border-[#8892b0] text-[#F5F5FA]'>Laila</h1>
+        <div>
+            <div name="persona" className='w-full mx-auto px-8 sm:px-32 flex flex-col items-center justify-center bg-[#1F1F21]'>
+                <h1 className='sm:text-4xl font-bold inline border-4 px-4 py-2 mt-8 tracking-wider bg-[#1F1F21] border-pink-600 text-[#F5F5FA]'>Persona</h1>
+                <h1 className='sm:text-2xl font-bold inline border-2 px-4 py-2 mt-8 tracking-wider bg-[#1F1F21] border-[#8892b0] text-[#F5F5FA]'>Laila</h1>
 
-            <div className='max-w-[1200px] grid grid-cols-1 sm:grid-cols-3 gap-8 py-8'>
+                <div className='max-w-[1200px] grid grid-cols-1 sm:grid-cols-3 gap-8 py-8'>
 
-                <div name='col1' className='order-2 sm:order-2 mx-auto'>
-                    <div className='w-[300px] flex flex-col items-center justify-center border-2 border-[#F5F5FA] bg-[#9995C8] pb-8 mb-6'>
-                        <h1 className='sm:text-2xl font-bold inline border-2 px-4 py-2 my-4 tracking-wider bg-[#1F1F21] border-pink-600 text-[#F5F5FA]'>Pain Points</h1>
-                        <ul>
-                            <li>Always on the go</li>
-                            <li>Needs her coffee, but running short on time</li>
-                            <li>Wants to give back, but cant find time</li>
-                            <li>Would prefer to support local non-profit, than a large chain</li>
+                    <div name='col1' className='order-2 sm:order-2 mx-auto'>
+                        <div className='w-[300px] flex flex-col items-center justify-center border-2 border-[#F5F5FA] bg-[#9995C8] pb-8 mb-6'>
+                            <h1 className='sm:text-2xl font-bold inline border-2 px-4 py-2 my-4 tracking-wider bg-[#1F1F21] border-pink-600 text-[#F5F5FA]'>Pain Points</h1>
+                            <ul>
+                                <li>Always on the go</li>
+                                <li>Needs her coffee, but running short on time</li>
+                                <li>Wants to give back, but cant find time</li>
+                                <li>Would prefer to support local non-profit, than a large chain</li>
+                            </ul>
+                        </div>
+                        <div className='w-[300px] flex flex-col items-center justify-center border-2 border-[#F5F5FA] bg-[#9995C8] pb-8'>
+                            <h1 className='max-w-[200px] sm:text-2xl inline border-2 px-4 py-2 my-4 tracking-wider bg-[#1F1F21] border-pink-600 text-[#F5F5FA]'>Personality & Known Habits</h1>
+                            <ul className='py-2'>
+                                <li>Independent</li>
+                                <li>Compassionate</li>
+                                <li>Animal Lover</li>
+                                <li>Values local /sustainable businesses</li>
+                                <li>Appreciates the arts</li>
+                                <li>Values Community</li>
+                                <li>Cares about Environment</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div name='col2' className='order-1 sm:order-2 mx-auto'>
+                        <img className='max-h-[368px] mx-auto rounded-full border-2 p-4' src={laila} alt='Laila' />
+                        <ul className='w-[300px] bg-[#9995C8] py-4 mt-8 border-2 border-[#F5F5FA]'>
+                            <li><span className='font-bold'>Age:</span> 33</li>
+                            <li><span className='font-bold'>Education:</span> College Graduate</li>
+                            <li><span className='font-bold'>Occupation:</span> Project Manager</li>
+                            <li><span className='font-bold'>Location:</span> San Jose, CA</li>
+                            <li><span className='font-bold'>Neighborhood:</span> Urban Tech</li>
                         </ul>
                     </div>
-                    <div className='w-[300px] flex flex-col items-center justify-center border-2 border-[#F5F5FA] bg-[#9995C8] pb-8'>
-                        <h1 className='max-w-[200px] sm:text-2xl inline border-2 px-4 py-2 my-4 tracking-wider bg-[#1F1F21] border-pink-600 text-[#F5F5FA]'>Personality & Known Habits</h1>
-                        <ul className='py-2'>
-                            <li>Independent</li>
-                            <li>Compassionate</li>
-                            <li>Animal Lover</li>
-                            <li>Values local /sustainable businesses</li>
-                            <li>Appreciates the arts</li>
-                            <li>Values Community</li>
-                            <li>Cares about Environment</li>
-                        </ul>
+
+                    <div name='col3' className='order-3'>
+                        <div className='w-[300px] flex flex-col items-center justify-center border-2 border-[#F5F5FA] bg-[#9995C8] pb-8 mb-6'>
+                            <h1 className='sm:text-2xl font-bold inline border-2 px-4 py-2 my-4 tracking-wider bg-[#1F1F21] border-pink-600 text-[#F5F5FA]'>Goals</h1>
+                            <ul>
+                                <li>Order and pay for coffee ahead of time</li>
+                                <li>Pickup morning coffee</li>
+                                <li>Donate to good causes</li>
+                                <li>Spend time with animals</li>
+                                <li>Support small businesses</li>
+                            </ul>
+                        </div>
+                        <div className='w-[300px] flex flex-col items-center justify-center border-2 border-[#F5F5FA] bg-[#9995C8] pb-8'>
+                            <h1 className='max-w-[200px] sm:text-2xl inline border-2 px-4 py-2 my-4 tracking-wider bg-[#1F1F21] border-pink-600 text-[#F5F5FA]'>Bio </h1>
+                            <p className='px-4'>Laila is a busy professional. She needs her coffee but is also conscious about where her money goes and would prefer to support small businesses or preferably a non-profit. Laila has a busy job and wishes her apartment would allow her to have a pet. She has heard about cat cafes and thinks this would be a good way to get her cat fix. </p>
+                        </div>
                     </div>
                 </div>
 
-                <div name='col2' className='order-1 sm:order-2 mx-auto'>
-                    <img className='max-h-[368px] mx-auto rounded-full border-2 p-4' src={laila} alt='Laila' />
-                    <ul className='w-[300px] bg-[#9995C8] py-4 mt-8 border-2 border-[#F5F5FA]'>
-                        <li><span className='font-bold'>Age:</span> 33</li>
-                        <li><span className='font-bold'>Education:</span> College Graduate</li>
-                        <li><span className='font-bold'>Occupation:</span> Project Manager</li>
-                        <li><span className='font-bold'>Location:</span> San Jose, CA</li>
-                        <li><span className='font-bold'>Neighborhood:</span> Urban Tech</li>
-                    </ul>
-                </div>
-
-                <div name='col3' className='order-3'>
-                    <div className='w-[300px] flex flex-col items-center justify-center border-2 border-[#F5F5FA] bg-[#9995C8] pb-8 mb-6'>
-                        <h1 className='sm:text-2xl font-bold inline border-2 px-4 py-2 my-4 tracking-wider bg-[#1F1F21] border-pink-600 text-[#F5F5FA]'>Goals</h1>
-                        <ul>
-                            <li>Order and pay for coffee ahead of time</li>
-                            <li>Pickup morning coffee</li>
-                            <li>Donate to good causes</li>
-                            <li>Spend time with animals</li>
-                            <li>Support small businesses</li>
-                        </ul>
-                    </div>
-                    <div className='w-[300px] flex flex-col items-center justify-center border-2 border-[#F5F5FA] bg-[#9995C8] pb-8'>
-                        <h1 className='max-w-[200px] sm:text-2xl inline border-2 px-4 py-2 my-4 tracking-wider bg-[#1F1F21] border-pink-600 text-[#F5F5FA]'>Bio </h1>
-                        <p className='px-4'>Laila is a busy professional. She needs her coffee but is also conscious about where her money goes and would prefer to support small businesses or preferably a non-profit. Laila has a busy job and wishes her apartment would allow her to have a pet. She has heard about cat cafes and thinks this would be a good way to get her cat fix. </p>
-                    </div>
-                </div>
-            </div>
-
-
+            </div >
         </div >
     )
 }
--- src/components/UXCaseStudyWW.jsx
+++ src/components/UXCaseStudyWW.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-
 import { HiArrowNarrowRight } from 'react-icons/hi'
 import ww from '../assets/images/WW.png'
 
@@ -8,62 +7,62 @@ const UXCaseStudyWW = () => {
 
   useEffect(() => {
     fetch('/index.php/api/caseStudyWW_JSON')
-        .then(response => response.json())
-        .then(data => setCSFields(data))
-        .catch(error => console.error('Error fetching data:', error));
+      .then(response => response.json())
+      .then(data => setCSFields(data))
+      .catch(error => console.error('Error fetching data:', error));
   }, []);
 
 
-  
+
   return (
     <div name='uxhero' className=' flex flex-col justify-center bg-[#9995C8]'>
       {caseStudy ? (
-      <div className='w-full mx-auto px-8 sm:px-32 bg-[#1F1F21]'>
-        <div className='max-w-[1200px] w-full grid grid-cols-1 md:grid-cols-2 mt-40 gap-8 py-8 '>
-          
-          <div>
-            <h2 className='text-pink-600 text-2xl sm:text-5xl font-bold'>{caseStudy.title}</h2>
-            <h2 className='text-white text-1xl sm:text-2xl font-bold py-4'>{caseStudy.features}</h2>
-            <p className='text-[#8892b0] py-4 max-w-[700px]'>{caseStudy.project_intro}</p>
-            <button className='max-w-[200px] text-white bg-black border-2 border-white-300 px-6 py-3 my-4 flex items-center hover:bg-pink-600 border-pink-600'
-              onClick={() => window.location.href = 'https://www.figma.com/proto/tHheHUfhaYj3cZ4dmacK2O/WW-wireframes?type=design&node-id=0-1&t=WefZLjA7IWc2UHDZ-0&scaling=contain&starting-point-node-id=34%3A1270&show-proto-sidebar=1'}>Prototype {caseStudy.project_url}
-              <span className='hover:pl-4 duration-300'>
-                <HiArrowNarrowRight className='ml-3' />
-              </span>
-            </button>
-          </div>
-          
+        <div className='w-full mx-auto px-8 sm:px-32 bg-[#1F1F21]'>
+          <div className='max-w-[1200px] w-full grid grid-cols-1 md:grid-cols-2 mt-40 gap-8 py-8 '>
 
-          <div>
-            <img className='mx-auto p-4 cursor-pointer' onClick={() => window.location.href = 'https://www.figma.com/proto/tHheHUfhaYj3cZ4dmacK2O/WW-wireframes?type=design&node-id=0-1&t=WefZLjA7IWc2UHDZ-0&scaling=contain&starting-point-node-id=34%3A1270&show-proto-sidebar=1'} src={ww} alt='' />
-          </div>
-        </div>
+            <div>
+              <h2 className='text-pink-600 text-2xl sm:text-5xl font-bold'>{caseStudy.title}</h2>
+              <h2 className='text-white text-1xl sm:text-2xl font-bold py-4'>{caseStudy.features}</h2>
+              <p className='text-[#8892b0] py-4 max-w-[700px]'>{caseStudy.project_intro}</p>
+              <button className='max-w-[200px] text-white bg-black border-2 border-white-300 px-6 py-3 my-4 flex items-center hover:bg-pink-600 border-pink-600'
+                onClick={() => window.location.href = 'https://www.figma.com/proto/tHheHUfhaYj3cZ4dmacK2O/WW-wireframes?type=design&node-id=0-1&t=WefZLjA7IWc2UHDZ-0&scaling=contain&starting-point-node-id=34%3A1270&show-proto-sidebar=1'}>Prototype {caseStudy.project_url}
+                <span className='hover:pl-4 duration-300'>
+                  <HiArrowNarrowRight className='ml-3' />
+                </span>
+              </button>
+            </div>
 
-        <div className='max-w-[1200px] w-full grid grid-cols-1 md:grid-cols-2 gap-8 py-8'>
-          <div>
-            <h1 className='text-2xl font-bold inline py-2 text-[#F5F5FA]'>Challenge</h1>
-            <p className='text-[#8892b0] py-4 max-w-[700px]'>{caseStudy.challenge}</p>
+
+            <div>
+              <img className='mx-auto p-4 cursor-pointer' onClick={() => window.location.href = 'https://www.figma.com/proto/tHheHUfhaYj3cZ4dmacK2O/WW-wireframes?type=design&node-id=0-1&t=WefZLjA7IWc2UHDZ-0&scaling=contain&starting-point-node-id=34%3A1270&show-proto-sidebar=1'} src={ww} alt='' />
+            </div>
           </div>
-          <div>
-            <h1 className='text-2xl font-bold inline py-2 text-[#F5F5FA]'>Role</h1>
-            <p className='text-[#8892b0] py-4 max-w-[700px]'>{caseStudy.role}</p>
+
+          <div className='max-w-[1200px] w-full grid grid-cols-1 md:grid-cols-2 gap-8 py-8'>
+            <div>
+              <h1 className='text-2xl font-bold inline py-2 text-[#F5F5FA]'>Challenge</h1>
+              <p className='text-[#8892b0] py-4 max-w-[700px]'>{caseStudy.challenge}</p>
+            </div>
+            <div>
+              <h1 className='text-2xl font-bold inline py-2 text-[#F5F5FA]'>Role</h1>
+              <p className='text-[#8892b0] py-4 max-w-[700px]'>{caseStudy.role}</p>
+            </div>
+            <div>
+              <h1 className='text-2xl font-bold inline py-2 text-[#F5F5FA]'>Tools</h1>
+              <p className='text-[#8892b0] py-4 max-w-[700px]'>{caseStudy.tools}</p>
+            </div>
+            <div>
+              <h1 className='text-2xl font-bold inline py-2 text-[#F5F5FA]'>Deliverables</h1>
+              <p className='text-[#8892b0] py-4 max-w-[700px]'>{caseStudy.deliverables}</p>
+            </div>
+            <div>
+              <h1 className='text-2xl font-bold inline py-2 text-[#F5F5FA]'>Timeline</h1>
+              <p className='text-[#8892b0] py-4 max-w-[700px]'>{caseStudy.timeline}</p>
+            </div>
           </div>
-          <div>
-            <h1 className='text-2xl font-bold inline py-2 text-[#F5F5FA]'>Tools</h1>
-            <p className='text-[#8892b0] py-4 max-w-[700px]'>{caseStudy.tools}</p>
-          </div>
-          <div>
-            <h1 className='text-2xl font-bold inline py-2 text-[#F5F5FA]'>Deliverables</h1>
-            <p className='text-[#8892b0] py-4 max-w-[700px]'>{caseStudy.deliverables}</p>
-          </div>
-          <div>
-            <h1 className='text-2xl font-bold inline py-2 text-[#F5F5FA]'>Timeline</h1>
-            <p className='text-[#8892b0] py-4 max-w-[700px]'>{caseStudy.timeline}</p>
-          </div>
+
         </div>
-        
-      </div>
-      ) : ( <p>Loading case study...</p>)}
+      ) : (<p>Loading case study...</p>)}
     </div>
   )
 }
